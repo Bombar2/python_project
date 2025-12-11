032c6b57f08deed34e63de20a7d7d4b12968d7f1
Mikhai.Khramov <mikhai.khramov@gmail.com>
2025-12-11 22:25:52 +0300

    Добавил покупки.

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,26 +1,38 @@
 import random as ran
+from enum import Enum
 
+class Mode(Enum):
+    ADD = 1
+    REMOVE = 2
+    RELOAD = 3
 
 class GuessTheNumber:
     __HEALTH = 3
     __COIN = 0
 
     def __init__(self, min_number: int = 0, max_number: int = 100):
-        self.__random_number = ran.uniform(min_number, max_number)
         self.__coin = 0
         self.__health = 3
+        self.min_number = min_number
+        self.max_number = max_number
+        self.__random_number = ran.randint(min_number, max_number)
 
     @property
     def random_number(self):
         """getter for random number"""
         return self.__random_number
 
+    @random_number.setter
+    def random_number(self, value: int = None):
+        """setter for random number"""
+        self.__random_number = ran.randint(self.min_number, self.max_number)
+
     @property
     def coin(self):
         return self.__coin
 
     @coin.setter
-    def coin(self, *, value = None):
+    def coin(self, value: int = None):
         """setter for coin"""
         if value is not None:
             self.__coin += value
@@ -32,19 +44,10 @@ class GuessTheNumber:
         return self.__health
 
     @health.setter
-    def health(self, *, value = None):
-        if value is not None:
+    def health(self, *, value = None, mode: Mode = Mode.REMOVE):
+        if value is not None and mode == Mode.REMOVE:
             self.__health -= value
-        else:
+        elif mode == Mode.RELOAD:
             self.__health = self.__HEALTH
-
-
-    def game(self):
-        print("Guess the number. Добро пожаловать в игру 'Угадай число'!")
-        print("Правила очень просты. Машина загадывает, ты угадываешь!")
-        print("За каждое угажанное число с трех попыток начисляется балы!")
-        print("За каждый проигрышь, снимаются очки жизни. Всего их три!")
-
-        human_number = input("Введите число... ")
-
-        print(human_number)
+        else:
+            self.__HEALTH += 1
--- main.py
+++ main.py
@@ -1,11 +1,56 @@
 from guess_the_number import GuessTheNumber
 
 
-def gtn():
-    g = GuessTheNumber(min_number=1, max_number=100)
-    g.game()
+def game():
+    gtn = GuessTheNumber()
+    random_number = gtn.random_number
+
+    print("Guess the number. Добро пожаловать в игру 'Угадай число'!")
+    print("Правила очень просты. Машина загадывает, ты угадываешь!")
+    print("За каждое угажанное число с трех попыток начисляется балы!")
+    print("За каждый проигрышь, снимаются очки жизни. Всего их три!")
+
+    while True:
+        print(f"Угадываемое число {random_number}")
+        human_number = int(input("Введите число... "))
+
+        if human_number == random_number:
+            gtn.coin = 1
+            print(f"Угадал! Плюс бал! Сейчас на балансе {gtn.coin}")
+
+            while True:
+                print("Покупки.")
+                if gtn.coin >= 1:
+                    print("1. Восстановить здоровье на 1 - 1 бал")
+                if gtn.coin >= 5:
+                    print("2. Купить дополнительную попытку для угадывания - 5 балов")
+                    print("3. Купить дополнительное здоровье - 5 балов")
+                if gtn.coin >= 10:
+                    print("4. Сохранить игру - 10 балов")
+                print("Хотите совершить покупку?")
+                answer = str(input("Введите для ответа Д/Н "))
+                if answer.lower() == "н" or answer.lower() == "не" or answer.lower() == "нет":
+                    break
+
+                while answer == "Д":
+                    coin = int(input("Введите количество балов, которое хотите потратить."))
+                    answer = int(input("Введите номер раздела, который вы хотите оплатить."))
+
+                    if coin > gtn.coin:
+                        print("У вас нет такого количества балов. Попробуйте ввести количество балов снова.")
+                    else:
+                        match answer:
+                            case 1:
+                                gtn.health = 1
+                            case 2:
+
+
+
+
+
+
 
 if __name__ == '__main__':
-    gtn()
+    game()
 
 
