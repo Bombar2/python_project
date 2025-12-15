854a412a6636925610df1b839ad5f9808457ba4f
Mikhai.Khramov <mikhai.khramov@gmail.com>
2025-12-15 21:32:45 +0300

    Дописал алгоритм обработки ответов.

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,53 +1,55 @@
 import random as ran
-from enum import Enum
-
-class Mode(Enum):
-    ADD = 1
-    REMOVE = 2
-    RELOAD = 3
 
 class GuessTheNumber:
     __HEALTH = 3
     __COIN = 0
+    __ATTEMPT = 3
 
-    def __init__(self, min_number: int = 0, max_number: int = 100):
-        self.__coin = 0
-        self.__health = 3
+    def __init__(self, min_number: int = 0, max_number: int = 5):
+        self.__coin = self.__COIN
+        self.__health = self.__HEALTH
+        self.__attempt = self.__ATTEMPT
         self.min_number = min_number
         self.max_number = max_number
-        self.__random_number = ran.randint(min_number, max_number)
+        self.__random_number = int(ran.randint(self.min_number, self.max_number))
+
+    def reload(self):
+        """Перезапуск если проиграли"""
+        self.__coin = self.__COIN
+        self.__health = self.__HEALTH
+        self.__attempt = self.__ATTEMPT
+        self.__random_number = int(ran.randint(self.min_number, self.max_number))
 
     @property
     def random_number(self):
         """getter for random number"""
         return self.__random_number
 
-    @random_number.setter
-    def random_number(self, value: int = None):
-        """setter for random number"""
-        self.__random_number = ran.randint(self.min_number, self.max_number)
-
     @property
     def coin(self):
         return self.__coin
 
     @coin.setter
-    def coin(self, value: int = None):
+    def coin(self, value: int):
         """setter for coin"""
-        if value is not None:
-            self.__coin += value
-        else:
-            self.__coin = self.__COIN
+        self.__coin += value
 
     @property
     def health(self):
         return self.__health
 
     @health.setter
-    def health(self, *, value = None, mode: Mode = Mode.REMOVE):
-        if value is not None and mode == Mode.REMOVE:
-            self.__health -= value
-        elif mode == Mode.RELOAD:
-            self.__health = self.__HEALTH
-        else:
-            self.__HEALTH += 1
+    def health(self, value: int):
+        self.__health += value
+
+    @property
+    def attempt(self):
+        return self.__attempt
+
+    @attempt.setter
+    def attempt(self, value: int):
+        """setter for attempt"""
+        self.__attempt += value
+
+    def reload_attempt(self):
+        self.__attempt = self.__ATTEMPT
--- main.py
+++ main.py
@@ -3,48 +3,51 @@ from guess_the_number import GuessTheNumber
 
 def game():
     gtn = GuessTheNumber()
-    random_number = gtn.random_number
+
 
     print("Guess the number. Добро пожаловать в игру 'Угадай число'!")
     print("Правила очень просты. Машина загадывает, ты угадываешь!")
     print("За каждое угажанное число с трех попыток начисляется балы!")
     print("За каждый проигрышь, снимаются очки жизни. Всего их три!")
 
+
+    end_game = False
     while True:
-        print(f"Угадываемое число {random_number}")
-        human_number = int(input("Введите число... "))
+        random_number = -1
+        random_number = gtn.random_number
 
-        if human_number == random_number:
-            gtn.coin = 1
-            print(f"Угадал! Плюс бал! Сейчас на балансе {gtn.coin}")
+        while True:
+            print(f"Угадываемое число {random_number}")
+            human_number = int(input("Введите число... "))
 
-            while True:
-                print("Покупки.")
-                if gtn.coin >= 1:
-                    print("1. Восстановить здоровье на 1 - 1 бал")
-                if gtn.coin >= 5:
-                    print("2. Купить дополнительную попытку для угадывания - 5 балов")
-                    print("3. Купить дополнительное здоровье - 5 балов")
-                if gtn.coin >= 10:
-                    print("4. Сохранить игру - 10 балов")
-                print("Хотите совершить покупку?")
-                answer = str(input("Введите для ответа Д/Н "))
-                if answer.lower() == "н" or answer.lower() == "не" or answer.lower() == "нет":
-                    break
+            if human_number == random_number:
+                gtn.coin = 1
+                gtn.reload_attempt()
+                print(f"Угадал! Плюс бал! Сейчас на балансе {gtn.coin}")
 
-                while answer == "Д":
-                    coin = int(input("Введите количество балов, которое хотите потратить."))
-                    answer = int(input("Введите номер раздела, который вы хотите оплатить."))
+            elif human_number != random_number:
+                if human_number > random_number:
+                    print(f"Введённое число больше загаданного!")
+                elif human_number < random_number:
+                    print(f"Введённое число меньше загаданного!")
+                gtn.attempt = -1
 
-                    if coin > gtn.coin:
-                        print("У вас нет такого количества балов. Попробуйте ввести количество балов снова.")
-                    else:
-                        match answer:
-                            case 1:
-                                gtn.health = 1
-                            case 2:
+                if gtn.attempt == 0:
+                    gtn.health = -1
+                    print(f"У вас закончились попытки. Осталось {gtn.health} XP!")
+                    gtn.reload_attempt()
+                    if gtn.health == 0:
+                        print(f"У вас не осталось XP!")
+                        answer = input("Хотите начть игру заново? Напишите ответ Д/Н... ")
+                        if answer.lower() == "н" or answer.lower() == "не" or answer.lower() == "нет":
+                            end_game = True
+                            break
+                        elif answer.lower() == "д" or answer.lower() == "да":
+                            gtn.reload()
+                            break
 
-
+        if end_game:
+            break
 
 
 
@@ -53,4 +56,47 @@ def game():
 if __name__ == '__main__':
     game()
 
+
+def pocupci(gtn: GuessTheNumber):
+    while True:
+
+        if gtn.coin == 0:
+            break
+
+        print("Покупки.")
+        if gtn.coin >= 1:
+            print("1. Восстановить здоровье на 1 - 1 бал")
+        if gtn.coin >= 5:
+            print("2. Купить дополнительную попытку для угадывания - 5 балов")
+            print("3. Купить дополнительное здоровье - 5 балов")
+        if gtn.coin >= 10:
+            print("4. Сохранить игру - 10 балов")
+        print("Хотите совершить покупку?")
+        answer = str(input("Введите для ответа Д/Н "))
+        if answer.lower() == "н" or answer.lower() == "не" or answer.lower() == "нет":
+            break
+
+        while answer.lower() == "д" or answer.lower() == "да":
+            coin = int(input(f"Сейчас у вас {gtn.coin} бал(ов). "
+                             f"Введите количество балов, которое хотите потратить... "))
+            answer_buy = int(input("Введите номер раздела, который вы хотите оплатить... "))
+
+            if coin > gtn.coin:
+                print("У вас нет такого количества балов. Попробуйте ввести количество балов снова.")
+            else:
+                match answer_buy:
+                    case 1:
+                        gtn.health = 1
+                        gtn.coin = -1
+                        break
+                    case 2:
+                        gtn.attempt = 1
+                        gtn.coin = -5
+                        break
+                    case 3:
+                        gtn.health = None
+                        gtn.coin = -5
+                        break
+                    case 4:
+                        print("Функция ещё не добавлена! Ожидайте последующих обновлений!")
 
